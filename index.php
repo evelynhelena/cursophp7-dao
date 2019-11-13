9a5ecf813089e943304055e1dfd8ce6dda327101
Evelyn <evelyn.helena1@gmail.com>
2019-11-13 10:02:23 -0300

    Atualizando

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+
+
+/*$root = new Usuario(); // traz um usuario só
 $root->loadbyId(10);
-echo $root;
+echo $root;*/
 
 
 /*$sql = new Sql();
@@ -12,5 +14,16 @@ echo $root;
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
+
+/*$lista = Usuario::getList();//lista todos os usuarios do banco
+echo json_encode($lista);*/
+
+/*$busca = Usuario::search("Ev");// traz todos os usurios que começam com EV
+echo json_encode($busca);*/
+
+$usuario = new Usuario();
+$usuario->login("Evelyn","123456");
+
+echo ($usuario);
 
 ?>
